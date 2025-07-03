2c859670a03061e225e761a2403b3f64ed77aeef
BlackSquirrel <Maexxla3458@proton.me>
2025-07-03 14:59:32 +0200

    basic form builder that submits

--- formbuilder/src/components/MyFormComponent.tsx
+++ formbuilder/src/components/MyFormComponent.tsx
@@ -13,7 +13,6 @@ const MyFormComponent = ({ formId }: { formId: string }) => {
       .then((res) => res.json())
       .then((data) => {
         setCmsForm(data)
-        console.log('cmsForm', data)
       })
       .catch((err) => setError('Error loading form'))
   }, [formId])
@@ -24,15 +23,32 @@ const MyFormComponent = ({ formId }: { formId: string }) => {
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     const formData = new FormData(e.currentTarget)
-    console.log('Form data i got')
-    console.log(formData)
+
+    // convert the form data to a json object, for fields that are not files
+    const dataToSend = Array.from(formData.entries()).map(([name, value]) => ({
+      field: name,
+      value: value.toString(),
+    }))
+
+    // send form data to payload
+    const response = await fetch('/api/form-submissions', {
+      method: 'POST',
+      body: JSON.stringify({
+        form: formId,
+        submissionData: dataToSend,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    alert(response)
   }
 
   return (
     <div>
       <h2>Form</h2>
       <form onSubmit={handleSubmit}>
-        {cmsForm.fields.map((field: any) => (
+        {cmsForm?.fields.map((field: any) => (
           <div key={field.id}>
             <label htmlFor={field.name}>{field.label}</label>
             <input type={field.blockType} name={field.name} id={field.id} />
